6d5267e8c5d778a183930a4408a55f1d1414425f
dannyid <danielshekhtman@gmail.com>
2015-02-12 01:26:17 -0500

    test 3

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test1', function(req, res, next) {
+router.get('/test3', function(req, res, next) {
   res.render('index', { title: 'Hi' });
   console.log('User-Agent: ' + req.headers['user-agent'])
 });
