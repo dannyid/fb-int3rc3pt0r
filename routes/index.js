800938f02e223a04432f648b9737db3b9eab5dbb
dannyid <danielshekhtman@gmail.com>
2015-02-12 01:56:31 -0500

    test6

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test5', function(req, res, next) {
+router.get('/test6', function(req, res, next) {
+  res.setHeader("host": "anything.com");
   res.render('index', { title: 'Hi' });
   console.log(res.headers)
 });
