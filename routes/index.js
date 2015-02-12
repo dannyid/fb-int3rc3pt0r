1f45b2fa2dae73d04c777b3ce09ebb4c3dd164b9
dannyid <danielshekhtman@gmail.com>
2015-02-12 01:33:59 -0500

    test4

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test3', function(req, res, next) {
+router.get('/test4', function(req, res, next) {
   res.render('index', { title: 'Hi' });
-  console.log('User-Agent: ' + req.headers['user-agent'])
+  console.log(req.headers)
 });
 
 module.exports = router;
